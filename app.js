e59bf7efabc7025c7a8840768292fff3dfd3117c
iamvevaar <69046800+iamvevaar@users.noreply.github.com>
2023-09-14 20:42:05 +0000

    first home route

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const express = require("express");
 
-const app = express(PORT);
+const app = express();
+
+app.get("/" , (req , res)=>{
+    res.send("<h1>Hello hello heelo</h1>")
+})
 
 module.exports = app;
 
--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 const app = require("./app")
 const {PORT} = process.env; 
-app.listen(PORT , ()=>{console.log("server started")});
+app.listen(PORT , ()=>{console.log(`server started at ${PORT}`)});
 console.log("heelo saaab")
